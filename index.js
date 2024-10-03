8395d899cb8a65e5a2bed560fba68f9719f0cc92
Collins Kathurima <collinskathu313@gmail.com>
2024-10-03 11:23:50 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Write your solution here!
+const cats=["Milo", "Otis","Garfield"]
+    cats.length = 0
+    cats.push("Milo","Otis","Garfield");
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+    return cats;
+}
+cats.length = 0
+    cats.push("Milo","Otis","Garfield");
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+    return cats;
+}
+cats.length = 0
+    cats.push("Milo","Otis","Garfield");
+
+function destructivelyRemoveLastCat(){
+    cats.splice(-1);
+    return cats;
+}
+cats.length = 0
+     cats.push("Milo","Otis","Garfield");
+ function destructivelyRemoveFirstCat(){
+     cats.splice(0,1)
+     return cats;
+ }
+ cats.length = 0
+ cats.push("Milo","Otis","Garfield");
+ console.log(cats)
+function appendCat(name){
+    return [...cats, name]
+}
+cats.length = 0
+ cats.push("Milo","Otis","Garfield");
+ function prependCat(name){
+    return [name,...cats]
+}
+cats.length = 0
+ cats.push("Milo","Otis","Garfield");
+ function removeLastCat(name){
+    return cats.slice(0, -1)
+ }
+ cats.length = 0
+ cats.push("Milo","Otis","Garfield");
+ function removeFirstCat(name){
+    return cats.slice(1)
+ }
+ 
+
+
+
+
